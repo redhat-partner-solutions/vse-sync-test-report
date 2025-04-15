c1d12b7d475972fe36320406ee460412abd91a6b
Michele Costa <mtcosta@protonmail.com>
2025-04-15 13:39:57 +0100

    Remove query parameters from test_id

--- src/testdrive/asciidoc.py
+++ src/testdrive/asciidoc.py
@@ -26,7 +26,7 @@ class Config(dict):
             if test_id.startswith(baseurl):
                 return os.path.join(
                     self['repositories'][suite['repository']],
-                    test_id.split(baseurl, maxsplit=1)[1].lstrip('/'),
+                    test_id.split(baseurl, maxsplit=1)[1].split("?")[0].lstrip('/'),
                 )
         except KeyError:
             pass
